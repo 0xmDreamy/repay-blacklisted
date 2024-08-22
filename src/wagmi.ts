5978a57bd394812dc4c5b0026c1bb794f17a7424
0xmDreamy <0xmDreamy@proton.me>
2024-08-22 19:56:32 +0200

    add safe connector

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,12 @@
 import { http, createConfig } from "wagmi";
 import { mainnet } from "wagmi/chains";
-import { walletConnect } from "wagmi/connectors";
+import { safe, walletConnect } from "wagmi/connectors";
 
 export const config = createConfig({
 	chains: [mainnet],
 	connectors: [
 		walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+		safe(),
 	],
 	transports: {
 		[mainnet.id]: http(),
